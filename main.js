eef0c21a73d0cf8eb9e878066474dd4f62198c87
Shravani <shravanikolpe@2004gmail.com>
2025-11-04 09:09:56 +0530

    Third commit

--- main.js
+++ main.js
@@ -1,26 +1,60 @@
+let gamesPlayed = 0;
+let userWins = 0;
+let computerWins = 0;
+let gameDraw = 0;
 
 const userChoicePara = document.getElementById("user-choice-para");
 const computerChoicePara = document.getElementById("computer-choice-para");
 const resultPara = document.getElementById("result-para");
+const gamesCount = document.getElementById("games_count_para");
+const userScore = document.getElementById("user_score_para");
+const computerScore = document.getElementById("computer_score_para");
+const gamesDraw = document.getElementById("games_draw_para");
 
-function PlayGame(UserChoice) {
-    let options = ["rock", "paper", "scissors"];
-    const randomNum = Math.floor(Math.random() * 3); // 0,1,2
-    let computerChoice = options[randomNum];
+function playGame(UserChoice) {
+  let options = ["rock", "paper", "scissors"];
+  const randomNum = Math.floor(Math.random() * 3); // 0,1,2
+  let computerChoice = options[randomNum];
 
-    
-    userChoicePara.innerText = "User choice: " + UserChoice;
-    computerChoicePara.innerText = "Computer choice: " + computerChoice;
+  
+  userChoicePara.innerText = "User choice: " + UserChoice;
+  computerChoicePara.innerText = "Computer choice: " + computerChoice;
 
-    if (computerChoice === UserChoice) {
-        resultPara.innerText = "Result : It's a Draw 😊";
-    } else if (
-        (UserChoice === "rock" && computerChoice === "scissors") ||
-        (UserChoice === "paper" && computerChoice === "rock") ||
-        (UserChoice === "scissors" && computerChoice === "paper")
-    ) {
-        resultPara.innerText = "Result : You WON! 😍";
-    } else {
-        resultPara.innerText = "Result : You LOST! 😞";
-    }
+  
+  if (computerChoice === UserChoice) {
+    resultPara.innerText = "Result: It's a Draw 😊";
+    gameDraw++;
+  } else if (
+    (UserChoice === "rock" && computerChoice === "scissors") ||
+    (UserChoice === "paper" && computerChoice === "rock") ||
+    (UserChoice === "scissors" && computerChoice === "paper")
+  ) {
+    resultPara.innerText = "Result: You WON! 😍";
+    userWins++;
+  } else {
+    resultPara.innerText = "Result: You LOST! 😞";
+    computerWins++;
+  }
+
+  
+  gamesPlayed++;
+  gamesCount.innerText = "Games Played: " + gamesPlayed;
+  userScore.innerText = "Your Wins: " + userWins;
+  computerScore.innerText = "Computer Wins: " + computerWins;
+  gamesDraw.innerText = "Games Draw: " + gameDraw;
+}
+
+function resetGame() {
+  gamesPlayed = 0;
+  userWins = 0;
+  computerWins = 0;
+  gameDraw = 0;
+
+  gamesCount.innerText = "Games Played: " + gamesPlayed;
+  userScore.innerText = "Your Wins: " + userWins;
+  computerScore.innerText = "Computer Wins: " + computerWins;
+  userChoicePara.innerText = "User choice: ";
+  computerChoicePara.innerText = "Computer choice: ";
+  resultPara.innerText = "Result: ";
+  gamesDraw.innerText = "Games Draw: " + gameDraw;
 }
